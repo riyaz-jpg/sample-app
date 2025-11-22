fb7136f673ac5676735ae16c327a52dc20855056
root <root@ip-172-31-26-98.ap-southeast-2.compute.internal>
2025-11-22 07:03:32 +0000

    initial commit

---
+++ sample-app/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from ECS Fargate!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
